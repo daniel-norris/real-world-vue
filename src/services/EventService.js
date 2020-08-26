38984dd14be896c5946af993a3df45b839e5efd2
Daniel Norris <dan.norris@hotmail.com>
2020-08-26 18:14:18 +0100

    added actions

--- src/services/EventService.js
+++ src/services/EventService.js
@@ -18,5 +18,8 @@ export default {
     },
     getEvent(id) {
         return apiClient.get('/events/' + id);
+    },
+    postEvent(event) {
+        return apiClient.post('/events', event);
     }
 };
--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-
+import EventService from '@/services/EventService';
 Vue.use(Vuex);
 
 export default new Vuex.Store({
@@ -14,20 +14,34 @@ export default new Vuex.Store({
             'education',
             'food',
             'community'
+        ],
+        events: [
+            { id: 1, title: '...', organiser: '...' },
+            { id: 2, title: '...', organiser: '...' },
+            { id: 3, title: '...', organiser: '...' },
+            { id: 4, title: '...', organiser: '...' }
         ]
     },
-    mutations: {},
-    actions: {},
+    mutations: {
+        ADD_EVENT(state, event) {
+            state.events.push(event);
+        }
+    },
+    actions: {
+        createEvent({ commit }, event) {
+            // this prevents the store from being updated if the POST request does not complete
+            return EventService.postEvent(event).then(() => {
+                commit('ADD_EVENT', event);
+            });
+        }
+    },
     modules: {},
     getters: {
         catLength: state => {
             return state.categories.length;
         },
-        doneTodos: state => {
-            return state.todos.filter(todo => todo.done);
-        },
-        activeTodosCount: (state, getters) => {
-            return state.todos.length - getters.doneTodos.length;
+        getEventById: state => id => {
+            return state.events.find(event => event.id === id);
         }
     }
 });
